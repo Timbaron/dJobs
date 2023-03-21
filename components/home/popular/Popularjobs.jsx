50182ce1dcdb66c98c7f822b29b3722f039ed5cf
timbaron <timothy.akiode@student.aul.edu.ng>
2023-03-21 16:00:35 +0100

    add dynamic data to popular jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -12,11 +12,14 @@ import { COLORS, SIZES } from "../../../constants";
 import { useRouter } from "expo-router";
 
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const {data, isLoading, error} = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1,
+  });
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -32,7 +35,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
