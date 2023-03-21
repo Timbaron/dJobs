036e84d7a490f75c5cbfe3836ad54308259b8013
timbaron <timothy.akiode@student.aul.edu.ng>
2023-03-21 16:00:07 +0100

    Update popular job card design layout

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
+import { checkImageURL } from "../../../../utils";
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      opPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : "https://t3.ftcdn.net/jpg/04/93/13/42/360_F_493134256_DsdRygnyk1VflTXXuAjI211fWJqDLu1W.jpg",
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
