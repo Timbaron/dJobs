b69041b2fa4ffc836eebf9dd9da03da17c7c47ca
timbaron <timothy.akiode@student.aul.edu.ng>
2023-03-21 14:16:41 +0100

    Import costom fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,31 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+
+/* It prevents the splash screen from hiding automatically. */
+import * as SplashScreen from "expo-splash-screen";
+import { Text } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />;
-}
+  /* Loading the fonts and then hiding the splash screen. */
+  const [ fontsLoaded ] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
+  /* A function that is called when the root view is laid out. */
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  /* Checking if the fonts are loaded. If they are not, it returns null. If they are, it returns the
+    Stack. */
+  if (!fontsLoaded) return null;
+  return <Stack onLayout={onLayoutRootView} />;
+};
 
 export default Layout;
