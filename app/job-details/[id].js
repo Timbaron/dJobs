f70a92c1e872f450748d8495de1368949e66cbad
timbaron <timothy.akiode@student.aul.edu.ng>
2023-03-22 12:19:24 +0100

    Add job tabs to details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -15,20 +15,40 @@ import {
   JobFooter,
   JobTabs,
   ScreenHeaderBtn,
+  Specifics,
 } from "../../components";
 
 import { COLORS, SIZES, icons, images } from "../../constants";
 import useFetch from "../../hook/useFetch";
+
+const tabs = ["About", 'Qualifications', "Responsibilities"];
+
 const JobDetails = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
   const params = useSearchParams();
   const router = useRouter();
-
+  
   const { data, isLoading, error, refetch } = useFetch("job-details", {
     job_id: params.id,
   });
 
   const [refreshing, setRefreshing] = useState(false);
   const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return <Specifics title="Qualifications" points={data[0].job_highlights?.Qualifications ?? ['N/A'] } />
+      case "About":
+        return <JobAbout 
+          info={data[0].job_description ?? "No Data Available"}
+        />
+      case "Responsibilities":
+        return <Specifics title="Responsibilites" points={data[0].job_highlights?.Responsibilities ?? ['N/A'] } />
+      default: 
+        break;
+    }
+  }
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -70,10 +90,16 @@ const JobDetails = () => {
                 companyName={data[0].employer_name}
                 location={data[0].job_country}
               />
-              <JobTabs />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+        <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results'}/>
       </>
     </SafeAreaView>
   );
